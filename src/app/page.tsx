809a151ddd5e41f1089feb2abb49c5f2be39b11d
jkwamlah <jkwamlah@outlook.com>
2023-10-27 12:31:12 +0000

    next release

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,41 @@
 import Image from 'next/image'
 import Landing from "@/components/Landing";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+    getDefaultWallets,
+    RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    zora,
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen min-w-full flex-col items-center justify-between">
-      <Landing/>
-    </main>
-  )
+    const {chains, publicClient} = configureChains(
+        [mainnet, polygon, optimism, arbitrum, base, zora],
+        [
+            alchemyProvider({apiKey: process.env.ALCHEMY_ID}),
+            publicProvider()
+        ]
+    );
+    const {connectors} = getDefaultWallets({
+        appName: 'My RainbowKit App',
+        projectId: 'YOUR_PROJECT_ID',
+        chains
+    });
+
+
+    return (
+        <main className="flex min-h-screen min-w-full flex-col items-center justify-between">
+
+            <Landing/>
+        </main>
+    )
 }
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,7 +1,8 @@
 "use client"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const navigation = [
   { name: 'Product', href: '#' },
@@ -12,6 +13,12 @@ const navigation = [
 
 export default function Landing() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [userAddress, setUserAddress] = useState(null);
+
+  useEffect(() => {
+
+  }, [userAddress]); // Add userAddress as a dependency
+
 
   return (
     <div className="bg-gray-900 w-screen h-screen">
@@ -128,21 +135,19 @@ export default function Landing() {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              Data to enrich your online business
+              Ubuntu - Revolutionizing digital banking through customer loyalty.
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-300">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
               fugiat veniam occaecat fugiat aliqua.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
+              <ConnectButton />
               <a
                 href="#"
                 className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
               >
-                Get started
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-white">
-                Learn more <span aria-hidden="true">→</span>
+                Connect Wallet
               </a>
             </div>
           </div>
